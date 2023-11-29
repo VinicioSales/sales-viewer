bdf221ceb5741ee81347d883d43e5f1e90997978
VinicioSales <vinicio.santtana@gmail.com>
2023-11-29 16:35:46 -0300

    feat: att input dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginaAntecipacaoComponent } from './components/pagina-antecipacao/pagina-antecipacao.component';
 import { BotaoTemaComponent } from './components/botao-tema/botao-tema.component';
 import { BotaoHomeComponent } from './components/botao-home/botao-home.component';
+import { InputDropdownComponent } from './components/input-dropdown/input-dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaginaAntecipacaoComponent,
     BotaoTemaComponent,
-    BotaoHomeComponent
+    BotaoHomeComponent,
+    InputDropdownComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
