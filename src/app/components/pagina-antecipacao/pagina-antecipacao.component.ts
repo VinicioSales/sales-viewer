c454ffe95e418623d22ea8c0b07d50c5167a6318
VinicioSales <vinicio.santtana@gmail.com>
2023-12-05 11:36:25 -0300

    feat: Ajuste linhas

--- src/app/components/pagina-antecipacao/pagina-antecipacao.component.ts
+++ src/app/components/pagina-antecipacao/pagina-antecipacao.component.ts
@@ -8,13 +8,159 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagina-antecipacao.component.css']
 })
 export class PaginaAntecipacaoComponent {
+  corBotaoAdicionar: string = "var(--botao-verde)"
+  corBotaoAdicionarHover: string = "var(--botao-verde-hover)"
   vendas: any[] = [
     {
       numeroPedido: 123456,
       dataInclusao: "04/12/2023",
       previsaoFaturamento: "04/12/2023",
       valor: 500.00,
+      mostrarProdutos: false,
+      produtos: [
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+      ]
+    },
+    {
+      numeroPedido: 123456,
+      dataInclusao: "04/12/2023",
+      previsaoFaturamento: "04/12/2023",
+      valor: 500.00,
       mostrarProdutos: true,
+      produtos: [
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+      ]
+    },
+    {
+      numeroPedido: 123456,
+      dataInclusao: "04/12/2023",
+      previsaoFaturamento: "04/12/2023",
+      valor: 500.00,
+      mostrarProdutos: false,
+      produtos: [
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+      ]
+    },
+    {
+      numeroPedido: 123456,
+      dataInclusao: "04/12/2023",
+      previsaoFaturamento: "04/12/2023",
+      valor: 500.00,
+      mostrarProdutos: false,
+      produtos: [
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+      ]
+    },
+    {
+      numeroPedido: 123456,
+      dataInclusao: "04/12/2023",
+      previsaoFaturamento: "04/12/2023",
+      valor: 500.00,
+      mostrarProdutos: false,
+      produtos: [
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+      ]
+    },
+    {
+      numeroPedido: 123456,
+      dataInclusao: "04/12/2023",
+      previsaoFaturamento: "04/12/2023",
+      valor: 500.00,
+      mostrarProdutos: false,
+      produtos: [
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+        {
+          descricaoProduto: "Lorem ipsum dolor sit amet",
+          valorProduto: 100.00,
+          unidadeMedidaProduto: "UN"
+        },
+      ]
+    },
+    {
+      numeroPedido: 123456,
+      dataInclusao: "04/12/2023",
+      previsaoFaturamento: "04/12/2023",
+      valor: 500.00,
+      mostrarProdutos: false,
       produtos: [
         {
           descricaoProduto: "Lorem ipsum dolor sit amet",
