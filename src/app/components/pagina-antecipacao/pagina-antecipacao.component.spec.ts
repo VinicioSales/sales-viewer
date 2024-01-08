a286d40998531f09baecff1ff1e700c767ba29c3
VinicioSales <vinicio.santtana@gmail.com>
2024-01-08 15:11:00 -0300

    imports testes pagina antecipacao

--- src/app/components/pagina-antecipacao/pagina-antecipacao.component.spec.ts
+++ src/app/components/pagina-antecipacao/pagina-antecipacao.component.spec.ts
@@ -1,107 +1,79 @@
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ChangeDetectorRef } from '@angular/core';
 import { MockService } from 'src/app/mock/mock.service';
+import { HttpClientModule } from '@angular/common/http';
 import { InputComponent } from '../input/input.component';
 import { BotaoComponent } from '../botao/botao.component';
 import { LogService } from 'src/app/services/log/log.service';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BotaoTemaComponent } from '../botao-tema/botao-tema.component';
 import { BotaoHomeComponent } from '../botao-home/botao-home.component';
 import { PaginaAntecipacaoComponent } from './pagina-antecipacao.component';
 import { MensagensService } from 'src/app/services/mensagens/mensagens.service';
-import { BotaoDropdownComponent } from '../botao-dropdown/botao-dropdown.component';
 import { InputPesquisarFiltroComponent } from '../input-pesquisar-filtro/input-pesquisar-filtro.component';
 
-describe('PaginaAntecipacaoComponent', () => {
-  let component: PaginaAntecipacaoComponent;
-  let fixture: ComponentFixture<PaginaAntecipacaoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+fdescribe('PaginaAntecipacaoComponent', () => {
+  let component: PaginaAntecipacaoComponent;
+  let router: Router;
+  let cdr: ChangeDetectorRef;
+  let logService: LogService;
+  let mockService: MockService;
+  let mensagensService: MensagensService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [
-        FormsModule,
+        HttpClientModule,
         MatCheckboxModule,
-        RouterTestingModule,
-        HttpClientTestingModule,
       ],
       declarations: [
-        InputComponent,
         BotaoComponent,
+        InputComponent,
         BotaoTemaComponent,
         BotaoHomeComponent,
-        BotaoDropdownComponent,
         PaginaAntecipacaoComponent,
         InputPesquisarFiltroComponent,
       ],
       providers: [
+        Router,
         LogService,
         MockService,
-        MensagensService
-        // Se houver outros serviços que precisam de mocks, inclua-os aqui
-      ]
-    })
-    .compileComponents();
+        MensagensService,
+        ChangeDetectorRef,
+      ],
+    }).compileComponents();
+
+    component = TestBed.createComponent(PaginaAntecipacaoComponent).componentInstance;
+    router = TestBed.inject(Router);
+    cdr = TestBed.inject(ChangeDetectorRef);
+    logService = TestBed.inject(LogService);
+    mockService = TestBed.inject(MockService);
+    mensagensService = TestBed.inject(MensagensService);
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PaginaAntecipacaoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges(); // Inicializa o ciclo de detecção de mudanças do componente
-  });
-
-  it('should create', () => {
+  it('deve criar o componente', () => {
     expect(component).toBeTruthy();
   });
 
-  // // SECTION - constructor
-  // describe('PaginaAntecipacaoComponent', () => {
-  //   let component: PaginaAntecipacaoComponent;
-  //   let fixture: ComponentFixture<PaginaAntecipacaoComponent>;
-  //   let routerMock: Router;
-  //   let cdrMock: ChangeDetectorRef;
-  //   let logServiceMock: LogService;
-  //   let mockServiceMock: MockService;
-  //   let mensagensServiceMock: MensagensService;
+  it('deve injetar corretamente o serviço de roteamento', () => {
+    expect(router).toBeTruthy();
+  });
 
-  //   beforeEach(async () => {
-  //     routerMock = jasmine.createSpyObj('Router', ['navigate']);
-  //     cdrMock = jasmine.createSpyObj('ChangeDetectorRef', ['detectChanges']);
-  //     logServiceMock = jasmine.createSpyObj('LogService', ['log']);
-  //     mockServiceMock = jasmine.createSpyObj('MockService', ['getMockData']);
-  //     mensagensServiceMock = jasmine.createSpyObj('MensagensService', ['showMessage']);
-  
-  //     await TestBed.configureTestingModule({
-  //       imports: [HttpClientTestingModule], // Adicione esta linha
-  //       declarations: [ PaginaAntecipacaoComponent ],
-  //       providers: [
-  //         { provide: Router, useValue: routerMock },
-  //         { provide: ChangeDetectorRef, useValue: cdrMock },
-  //         { provide: LogService, useValue: logServiceMock },
-  //         { provide: MockService, useValue: mockServiceMock },
-  //         { provide: MensagensService, useValue: mensagensServiceMock },
-  //       ]
-  //     })
-  //     .compileComponents();
-  //   });
+  it('deve injetar corretamente o ChangeDetectorRef', () => {
+    expect(cdr).toBeTruthy();
+  });
 
-  //   beforeEach(() => {
-  //     fixture = TestBed.createComponent(PaginaAntecipacaoComponent);
-  //     component = fixture.componentInstance;
-  //     fixture.detectChanges();
-  //   });
+  it('deve injetar corretamente o LogService', () => {
+    expect(logService).toBeTruthy();
+  });
 
-  // // NOTE - deve instanciar corretamente
-  //   it('deve instanciar corretamente', () => {
-  //     expect(component).toBeTruthy();
-  //   });
-  // });
-  // //!SECTION
+  it('deve injetar corretamente o MockService', () => {
+    expect(mockService).toBeTruthy();
+  });
 
-
-
+  it('deve injetar corretamente o MensagensService', () => {
+    expect(mensagensService).toBeTruthy();
+  });
 });
