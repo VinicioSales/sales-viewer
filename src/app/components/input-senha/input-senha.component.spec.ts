4fcd3567fdfcdf98d7e9b334a483784481fb763f
VinicioSales <vinicio.santtana@gmail.com>
2024-01-15 16:17:40 -0300

    feat: Ajuste componente input-senha

--- src/app/components/input-senha/input-senha.component.spec.ts
+++ src/app/components/input-senha/input-senha.component.spec.ts
@@ -1,15 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InputSenhaComponent } from './input-senha.component';
+import { TemaService } from '../../services/tema/tema.service';
 
 describe('InputSenhaComponent', () => {
   let component: InputSenhaComponent;
   let fixture: ComponentFixture<InputSenhaComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [InputSenhaComponent],
+      providers: [TemaService]
+    })
+    .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [InputSenhaComponent]
-    });
     fixture = TestBed.createComponent(InputSenhaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +23,33 @@ describe('InputSenhaComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Testes de Inputs e Outputs', () => {
+    it('deve emitir evento valorChange ao chamar onValorChange', () => {
+      spyOn(component.valorChange, 'emit');
+      const valorTeste = 'teste';
+      component.onValorChange(valorTeste);
+      expect(component.valorChange.emit).toHaveBeenCalledWith(valorTeste);
+    });
+  });
+
+  describe('Testes de Alteração de Imagem', () => {
+    it('deve alternar a fonte da imagem ao chamar onClickVizualizacao', () => {
+      component.mostrarSenha = false;
+      component.onClickVizualizacao();
+      // Assumindo tema claro como padrão
+      const imgVisivelTemaClaro = 'assets/img/botao-visivel-light-mode.png';
+      expect(component.imgSrc).toEqual(imgVisivelTemaClaro);
+    });
+  });
+
+  describe('Testes de Emissão de Eventos', () => {
+    it('deve emitir evento mudarVizualizacaoSenha ao chamar onClickVizualizacao', () => {
+      spyOn(component.mudarVizualizacaoSenha, 'emit');
+      component.onClickVizualizacao();
+      expect(component.mudarVizualizacaoSenha.emit).toHaveBeenCalled();
+    });
+  });
+  
+  
 });
